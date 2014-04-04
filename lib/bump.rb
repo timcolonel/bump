1cb2dc25d38078198f8da707cbe4f47014c8955f
Timothee Guerin <timothee.guerin@outlook.com>
2014-04-04 16:31:13 -0400

    Load filename

--- lib/bump.rb
+++ lib/bump.rb
@@ -1,14 +1,23 @@
-require './lib/bump/config'
+require_relative 'bump/config'
 module Bump
   class << self
     def run
-      Bump::Config.version_path = '../rails_embed_editor/lib/rails_embed_editor/version.rb'
+      Bump::Config.version_filename = '../rails_embed_editor/lib/rails_embed_editor/version.rb'
       Bump::Config.version_regex = /(VERSION = ')(.*)(')/
-      puts 'Scan: ' + File.open(Bump::Config.version_path).read.scan(Bump::Config.version_regex).to_s
-      puts 'Scan: ' + File.open(Bump::Config.version_path).read.gsub(Bump::Config.version_regex, '\1replace_text\3').to_s
+      if File.open(Bump::Config.version_filename).read.match(Bump::Config.version_regex)
+        puts 'MATCHING'
+      end
+      Bump::Config.load_config
+      #update_version('0.0.2')
     end
 
 
+    def update_version(new_version)
+      change = File.open(Bump::Config.version_filename).read.gsub(Bump::Config.version_regex, '\1'+ new_version+'\3')
+      File.open(Bump::Config.version_filename, 'w') do |file|
+        file.write(change)
+      end
+    end
   end
 end
 
--- lib/bump/config.rb
+++ lib/bump/config.rb
@@ -1,10 +1,63 @@
+require 'yaml'
+
 module Bump
   module Config
+
+    CONFIG_FILE = 'bump.yml'
     class << self
+      #Application main language
+      attr_accessor :language
       #Path to the file containing the version variable
-      attr_accessor :version_path
+      attr_accessor :version_filename
       #Regex for the format the version is in the file
       attr_accessor :version_regex
+
+      attr_accessor :local_config
+
+      def data_dir
+        "#{File.dirname(__FILE__)}/../../data"
+      end
+
+      def load_config
+        @language = nil
+        @version_filename = nil
+        @version_regex = nil
+
+        load_local_config
+        if @language.nil?
+          @language = 'ruby'
+        end
+        if @version_filename.nil?
+          load_filename
+        end
+      end
+
+      def load_local_config
+        if File.exists?(CONFIG_FILE)
+          @local_config = load_file(CONFIG_FILE)
+          @language = @local_config['language']
+          @version_filename = @local_config['version_filename']
+          @version_regex = @local_config['version_regex']
+        end
+      end
+
+      def load_filename
+        languages = load_file(File.join(data_dir, 'version_filenames.yml'))
+        filenames = languages[@language] + languages['all']
+        filenames.each do |filename|
+          puts 'Trying bullshit: '+ filename + ' -- ' + Dir.glob(filename).to_s
+          results = Dir.glob(filename).size > 0
+          if results.size > 0
+            puts "Using: #{result[0]}"
+            @version_filename = result[0]
+          end
+        end
+        puts 'noediojio'
+      end
+
+      def load_file(filename)
+        YAML.load_file(filename)
+      end
     end
   end
 end
